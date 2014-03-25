65836f710b8506814936a16f2a6c503568737248
lohithkorp <lohith.korp@gmail.com>
2014-03-25 15:15:02 +0530

    Updated the account manager JS

--- www/accManager.js
+++ www/accManager.js
@@ -1,15 +1,30 @@
-var accManager =  
-   
-
-function(successCallback, errorCallback)
-{        
-cordova.exec(            
-successCallback, // success callback function            
-errorCallback, // error callback function            
-'accountmanager', // mapped to our native Java class called "accountmanager"            
-'accManager' // with this action name
-);
-}
+cordova.define("com.am.accountmanager.accountmanager", function(require, exports, module) 
+{ 
+var exec = require("cordova/exec");
 
 
-module.exports = accManager;
+function accManager() { }
+
+accManager.prototype.getLaunchParams = function (successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        };
+    }
+
+    if (typeof errorCallback != "function") {
+        console.log("accManager.getLaunchParams failure: failure parameter not a function");
+        return;
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("accManager.getLaunchParams failure: success callback parameter must be a function");
+        return;
+    }
+
+exec(successCallback,errorCallback,'accountmanager','accManager');
+};
+
+
+module.exports = new accManager();
+
+});
